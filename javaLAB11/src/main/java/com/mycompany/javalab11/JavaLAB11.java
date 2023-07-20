340b6311c5b2b455c34e7a5311665386faf600bd
WTBhanuka <137373304+WTBhanuka@users.noreply.github.com>
2023-07-20 22:27:03 +0530

    Add files via upload

---
+++ javaLAB11/src/main/java/com/mycompany/javalab11/JavaLAB11.java
@@ -0,0 +1,18 @@
+package com.mycompany.javalab11;
+
+
+import com.mycompany.javalab11.jframe1;
+
+
+
+
+
+
+public class JavaLAB11 {
+
+    public static void main(String[] args) 
+    {
+        jframe1 f1 = new jframe1();
+        f1.show();
+    }
+}
